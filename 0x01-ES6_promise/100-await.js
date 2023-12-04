cc1ad768a9aa22b66098a5d6009ea1f2aecd962e
Igomigo <igomigofatai@gmail.com>
2023-12-04 23:27:37 +0000

    Advanve=ced Tsksask done

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,22 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default async function asyncUploadUser() {
+  try {
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
+
+    // Use Promise.all to concurrently await both promises
+    const [photoResponse, userResponse] = await Promise.all([photoPromise, userPromise]);
+
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
