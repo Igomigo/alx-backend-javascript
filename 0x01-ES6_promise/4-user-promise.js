b19fa57a4ce623dc5cd0cbb3ed0e5cfb38e47659
Igomigo <igomigofatai@gmail.com>
2023-12-04 20:46:08 +0000

    Task 4 done

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,8 @@
 export default function signUpUser(firstName, lastName) {
-   return Promise.resolve({'firstname': firstName, 'lastName': lastName});
+   return new Promise((resolve) => {
+      resolve({
+         firstName,
+	 lastName,
+      });
+   });
 }
