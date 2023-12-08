915bffe01656933c793d599defb7625210b29f95
Igomigo <igomigofatai@gmail.com>
2023-12-08 05:42:30 +0000

    Taask 0 updated

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   return [
-    {id: 1, firstName: "Igomigo", location: "San Francisco"},
-    {id: 2, firstName: "Fatai", location: "San Francisco"},
-    {id: 5, firstName: "Guillaume", location: "Columbia"},
+    {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+    {id: 2, firstName: 'James', location: 'Columbia'},
+    {id: 5, firstName: 'Serena', location: 'San Francisco'},
   ];
 }
